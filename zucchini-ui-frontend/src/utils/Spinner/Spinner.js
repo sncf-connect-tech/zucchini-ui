d40af17bc0968853fb090e0f82b8ec5cd5a4d2b6
Axel Le Maitour <alemaitour@oui.sncf>
2019-08-21 15:54:48 +0200

    [Zucchini][Front] Ajout d'un infinite scroll, fix checkStyle

--- zucchini-ui-frontend/src/utils/Spinner/Spinner.js
+++ zucchini-ui-frontend/src/utils/Spinner/Spinner.js
@@ -2,16 +2,15 @@ import React from "react";
 // Libraries
 import PropTypes from "prop-types";
 // Styles
-import { GridLoader, ClipLoader } from "react-spinners";
+import { ClipLoader } from "react-spinners";
 import "./Spinner.css";
 
 const Spinner = props => (
   <div className="spinner-bottom">
-      <ClipLoader sizeUnit={"px"} size={50} color={"#5b5b5b"} loading={props.loading} />
+    <ClipLoader sizeUnit={"px"} size={50} color={"#5b5b5b"} loading={props.loading} />
   </div>
 );
 
 export default Spinner;
 
 Spinner.propTypes = { loading: PropTypes.bool };
-
