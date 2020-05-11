16bc3cb6f51906c8dc24b199b34d9c8e4a842273
Le palud Yves <ylepalud@oui.sncf>
2020-05-11 16:01:08 +0200

    Adding scenario and prediction queues

---
+++ zucchini-ui-backend/src/main/java/io/zucchiniui/backend/config/rabbitmq/PredictionQueue.java
@@ -0,0 +1,51 @@
+package io.zucchiniui.backend.config.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+@Component
+public class PredictionQueue {
+
+    public static final String PREDICTION_QUEUE = "zucchini-prediction";
+
+    public static final String PREDICTION_EXCHANGE = "eggplant-prediction";
+
+    public PredictionQueue() {
+        initPredictionChannel();
+    }
+
+    private void initPredictionChannel() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+            channel.exchangeDeclare(PREDICTION_EXCHANGE, "fanout", true);
+            channel.queueDeclare(PREDICTION_QUEUE, true, false, false, null);
+            channel.queueBind(PREDICTION_QUEUE, PREDICTION_EXCHANGE, "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        try {
+            channel.basicConsume(PREDICTION_QUEUE, true, deliverCallback, consumerTag -> { });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ zucchini-ui-backend/src/main/java/io/zucchiniui/backend/config/rabbitmq/ScenarioQueue.java
@@ -0,0 +1,61 @@
+package io.zucchiniui.backend.config.rabbitmq;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.zucchiniui.backend.scenario.domain.Scenario;
+import org.springframework.stereotype.Component;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+@Component
+public class ScenarioQueue {
+
+    public static final String SCENARIO_QUEUE = "eggplant-zucchini-incoming-scenario";
+
+    public static final String SCENARIO_EXCHANGE = "eggplant-zucchini-incoming-scenario";
+
+    public static final String NEW_SCENARIO_ROUTING_KEY = "new-scenario";
+
+    private Channel channel;
+
+    public ScenarioQueue() {
+        channel = initScenarioChannel();
+    }
+
+    private Channel initScenarioChannel() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Channel channel = null;
+        try {
+          Connection connection = factory.newConnection();
+          channel = connection.createChannel();
+          channel.exchangeDeclare(SCENARIO_EXCHANGE, "topic", true);
+          channel.queueDeclare(SCENARIO_QUEUE, true, false, false, null);
+          channel.queueBind(SCENARIO_QUEUE, SCENARIO_EXCHANGE, NEW_SCENARIO_ROUTING_KEY);
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return channel;
+    }
+
+    public void publishNewScenario(Scenario scenario) {
+        try {
+            this.channel.basicPublish(
+                SCENARIO_EXCHANGE,
+                NEW_SCENARIO_ROUTING_KEY,
+                false,
+                true,
+                null,
+                new ObjectMapper().writeValueAsString(scenario).getBytes()
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
